3e4cc00f07c56e0b91125b3f96f75bc01770b566
chucoyos <unodev@outlook.com>
2023-07-03 15:21:07 -0600

    Edit nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,12 +12,12 @@ const Nav = () => {
         <li>Blog</li>
         <li>Contact</li>
       </ul>
-      { location.pathname === '/' &&
+      { location.pathname === '/' ?
         <>
           <h1>Create your professional Resume with CV project</h1>
           <p className='subtitle'>Create your own professional resume and download it</p>
           <Button /> 
-        </>
+        </> : <h2 className='subtitle'>Personal Details</h2>
       }
     </nav>
   )
